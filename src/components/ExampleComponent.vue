d7c9cf47cd30339b816e89a6be22986abe7197f3
Daniel Vieira Vega <daniel.vega@me.com.br>
2024-11-03 11:37:50 -0300

    create the initial project

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+ <h1>Example</h1>
+</template>
+
+<script>
+export default {
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    caption: {
+      type: String,
+      default: '',
+    },
+
+    link: {
+      type: String,
+      default: '#',
+    },
+
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
