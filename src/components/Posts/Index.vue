ddcc073cb8de1d877df6da0ff830fada35191065
Daniel Vieira Vega <daniel.vega@me.com.br>
2024-11-10 14:03:56 -0300

    add animations like and description area

--- src/components/Posts/Index.vue
+++ src/components/Posts/Index.vue
@@ -18,23 +18,36 @@
       </q-icon>
     </div>
     <q-img
-      class="q-mt-sm"
+      class="q-mt-sm cursor-pointer"
       src="https://picsum.photos/500/300/"
       :ratio="1"
-    />
+      @click="countClicks++"
+    >
+      <q-icon
+        v-if="animationClass !== ''"
+        :class="animationClass"
+        class="absolute-center"
+        name="fas fa-heart"
+        color="white"
+      />
+    </q-img>
     <div class="row justify-between q-mx-sm q-my-md">
       <div>
         <q-img class="q-mr-md" width="24px" src="../../assets/heart.svg"></q-img>
         <q-img class="q-mr-md" width="22px" src="../../assets/comments.svg"></q-img>
         <q-img class="q-mr-md" width="25px" src="../../assets/direct.svg"></q-img>
       </div>
-      <q-icon
-        class="q-mr-md-xl"
-        name="fas fa-ellipsis-h"
-        size="20px"
-        color="dark-items">
-      </q-icon>
+      <q-icon class="q-mr-xl" name="fas fa-ellipsis-h" size="20px" color="dark-items"></q-icon>
       <q-img width="20px" src="../../assets/save-post.svg"></q-img>
+    </div>
+    <div class="row items-center q-mb-sm">
+      <q-avatar size="17px" class="q-ml-sm q-mr-xs">
+        <img src="https://cdn.quasar.dev/img/avatar.png">
+      </q-avatar>
+      Liked by <strong>craig_love</strong> and <strong>44,686 others</strong>
+    </div>
+    <div class="q-mx-sm q-mb-xl">
+      <strong>joshua_I</strong> The game in Japan was amazing and I want to share some photos
     </div>
   </div>
 </template>
@@ -42,5 +55,57 @@
 <script>
 export default {
   name: 'Post',
+  data() {
+    return {
+      animationClass: '',
+      countClicks: 0,
+    };
+  },
+  watch: {
+    countClicks() {
+      setTimeout(() => {
+        if (this.countClicks !== 2) {
+          this.countClicks = 0;
+        }
+      }, 1000);
+      if (this.countClicks === 2) {
+        this.addLikeInPost();
+        this.countClicks = 0;
+      }
+    },
+  },
+  methods: {
+    addLikeInPost() {
+      this.animationClass = 'animate-like';
+      setTimeout(() => {
+        this.animationClass = '';
+      }, 600);
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.animate-like {
+  animation-duration: 500ms;
+  animation-name: liked;
+}
+
+@keyframes liked {
+  from {
+    font-size: 45px;
+  }
+  10% {
+    font-size: 48px;
+  }
+  75% {
+    font-size: 75px;
+  }
+  90% {
+    font-size: 73px;
+  }
+  to {
+    font-size: 0px;
+  }
+}
+</style>
