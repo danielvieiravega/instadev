7fadd8b9f5dfdcc4a8f5caab5a4c958395822d9f
Daniel Vieira Vega <daniel.vega@me.com.br>
2024-11-17 17:29:21 -0300

    add router rules to control access to login and main pages based on token

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: 'sign-in', component: () => import('pages/SignIn/Index.vue'), alias: '' },
+      {
+        path: 'sign-in', name: 'login', component: () => import('pages/SignIn/Index.vue'), alias: '',
+      },
       { path: 'sign-up', component: () => import('pages/SignUp/Index.vue') },
       { path: 'my-area', component: () => import('pages/MyArea/Index.vue') },
-      { path: 'main', component: () => import('pages/Main/Index.vue') },
+      {
+        path: 'main', name: 'main', component: () => import('pages/Main/Index.vue'),
+      },
       { path: 'profile', component: () => import('pages/Profile/Index.vue') },
       { path: 'new-post', component: () => import('pages/NewPost/Index.vue') },
     ],
